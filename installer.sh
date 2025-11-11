9a2682c4996d965255fac85fa82db6ddb9359179
Wild_Hogs1948 <Hogwild1948@outlook.com>
2025-11-11 12:01:14 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -10,10 +10,10 @@ warn()   { echo -e "${YELLOW}[!]${RESET} $1"; }
 error()  { echo -e "${RED}[✖]${RESET} $1" >&2; exit 1; }
 
 if [[ $EUID -ne 0 ]]; then
-    error "Please run this script as root (sudo bash $0)"
+    error "Please run this script as root, yu retard (sudo bash $0)"
 fi
 
-log "Starting MushM Installer"
+log "Starting MushMTard Installer"
 
 CROSH="/usr/bin/crosh"
 MURK_DIR="/mnt/stateful_partition/murkmod"
@@ -27,20 +27,21 @@ mkdir -p "$MURK_DIR/mush"
 # touch "$MURK_DIR/mush/mushm.sh"
 mkdir -p "$MURK_DIR/plugins" "$MURK_DIR/pollen" || error "Failed to create MurkMod directories"
 
-log "Installing MushM"
-curl -fsSLo "$CROSH" "$MUSHM_URL" || error "Failed to download MushM"
+log "Installing MushMTard"
+curl -fsSLo "$CROSH" "$MUSHM_URL" || error "Failed to download MushMTard cause ur shit is fucked like you!"
 # curl -fsSLo "$MURK_DIR/mush/mushm.sh" "MUSHM_URL" || error "Failed to download MushM"
 
 if [[ -w "$(dirname "$BOOT_MSG")" ]]; then
     log "Customizing boot message..."
     cat <<'EOF' > "$BOOT_MSG"
-Oops, your system is Fucked up. We don't know why.
+Oops, your system is as Fucked as you,
 Hold tight while we try to repair it...
 EOF
 else
-    warn "Cannot modify boot message (permission denied or path missing)"
+    warn "Cannot modify boot message because your computer is fucked (permission denied or path missing retarded ass computer.)"
 fi
 
 log "Installation complete!"
 echo -e "${YELLOW}Made by Star_destroyer11${RESET}"
-echo -e "${GREEN}MushM installed. Have Fun${RESET}"
+echo -e "${RED}Modified by Jmoney46 (for retards)${RESET}"
+echo -e "${GREEN}MushMTard installed. Don't fuck shit up${RESET}"
